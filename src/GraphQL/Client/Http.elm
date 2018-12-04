c2abdc1cf8214a0db262c74e9c8685221a2f4189
klaftertief <jonas.coch@itravel.de>
2018-12-04 15:15:37 +0100

    Bring back tasks and support additional body parts

--- src/GraphQL/Client/Http.elm
+++ src/GraphQL/Client/Http.elm
@@ -1,4 +1,7 @@
-module GraphQL.Client.Http exposing (Error(..), RequestError, DocumentLocation, sendQuery, sendMutation, RequestOptions, customSendQuery, customSendMutation)
+module GraphQL.Client.Http exposing
+    ( Error(..), RequestError, DocumentLocation, sendQuery, sendMutation, RequestOptions, customSendQuery, customSendMutation
+    , customSendMutationTask, customSendMutationWithBodyParts, customSendQueryTask, sendMutationTask, sendQueryTask
+    )
 
 {-| The functions in this module let you perform HTTP requests to conventional GraphQL server endpoints.
 
@@ -69,7 +72,7 @@ sendQuery :
     -> Builder.Request Builder.Query result
     -> Cmd msg
 sendQuery =
-    defaultRequestOptions >> send
+    defaultRequestOptions >> send Nothing
 
 
 {-| Takes a URL and a `Mutation` `Request` and returns a `Task` that you can perform with `Task.attempt` which will send a `POST` request to a GraphQL server at the given endpoint.
@@ -80,7 +83,7 @@ sendMutation :
     -> Builder.Request Builder.Mutation result
     -> Cmd msg
 sendMutation =
-    defaultRequestOptions >> send
+    defaultRequestOptions >> send Nothing
 
 
 {-| Like `sendQuery`, but takes an `RequestOptions` value instead of a URL to let you further customize the HTTP request.
@@ -91,7 +94,7 @@ customSendQuery :
     -> Builder.Request Builder.Query result
     -> Cmd msg
 customSendQuery =
-    send
+    send Nothing
 
 
 {-| Like `sendMutation`, but takes an `RequestOptions` value instead of a URL to let you further customize the HTTP request.
@@ -102,25 +105,41 @@ customSendMutation :
     -> Builder.Request Builder.Mutation result
     -> Cmd msg
 customSendMutation =
+    send Nothing
+
+
+{-| Like `sendMutation`, but takes an `RequestOptions` value instead of a URL to let you further customize the HTTP request.
+-}
+customSendMutationWithBodyParts :
+    String
+    -> List Http.Part
+    -> RequestOptions
+    -> (Result Error result -> msg)
+    -> Builder.Request Builder.Mutation result
+    -> Cmd msg
+customSendMutationWithBodyParts jsonPartName parts =
     send
+        (Just
+            { jsonPartName = jsonPartName
+            , additionalParts = parts
+            }
+        )
+
+
+type alias BodyPartsConfig =
+    { jsonPartName : String
+    , additionalParts : List Http.Part
+    }
 
 
 send :
-    RequestOptions
+    Maybe BodyPartsConfig
+    -> RequestOptions
     -> (Result Error result -> msg)
     -> Builder.Request operationType result
     -> Cmd msg
-send requestOptions toMsg request =
+send maybeBodyPartsConfig requestOptions toMsg request =
     let
-        expect =
-            expectGraphQL toMsg request
-
-        documentString =
-            Builder.requestBody request
-
-        variableValues =
-            Builder.jsonVariableValues request
-
         httpRequest =
             if requestOptions.risky then
                 Http.riskyRequest
@@ -128,8 +147,69 @@ send requestOptions toMsg request =
             else
                 Http.request
     in
-    requestConfig requestOptions documentString expect variableValues
+    requestConfig maybeBodyPartsConfig requestOptions toMsg request
         |> httpRequest
+
+
+{-| -}
+sendQueryTask :
+    String
+    -> Builder.Request Builder.Query result
+    -> Task Error result
+sendQueryTask =
+    defaultRequestOptions >> task Nothing
+
+
+{-| -}
+sendMutationTask :
+    String
+    -> Builder.Request Builder.Mutation result
+    -> Task Error result
+sendMutationTask =
+    defaultRequestOptions >> task Nothing
+
+
+{-| -}
+customSendQueryTask :
+    RequestOptions
+    -> Builder.Request Builder.Query result
+    -> Task Error result
+customSendQueryTask =
+    task Nothing
+
+
+{-| -}
+customSendMutationTask :
+    RequestOptions
+    -> Builder.Request Builder.Mutation result
+    -> Task Error result
+customSendMutationTask =
+    task Nothing
+
+
+task :
+    Maybe BodyPartsConfig
+    -> RequestOptions
+    -> Builder.Request operationType result
+    -> Task Error result
+task maybeBodyPartsConfig requestOptions request =
+    let
+        httpTask =
+            if requestOptions.risky then
+                Http.riskyTask
+
+            else
+                Http.task
+    in
+    taskConfig maybeBodyPartsConfig requestOptions request
+        |> httpTask
+
+
+resolveGraphQL :
+    Builder.Request operationType result
+    -> Http.Resolver Error result
+resolveGraphQL request =
+    Http.stringResolver (responseToResult request)
 
 
 expectGraphQL :
@@ -137,37 +217,43 @@ expectGraphQL :
     -> Builder.Request operationType result
     -> Http.Expect msg
 expectGraphQL toMsg request =
+    Http.expectStringResponse toMsg (responseToResult request)
+
+
+responseToResult : Builder.Request operationType result -> Http.Response String -> Result Error result
+responseToResult request response =
     let
         decoder =
             Json.Decode.map2 Tuple.pair
-                (Json.Decode.maybe (Json.Decode.field "errors" Response.errorsDecoder))
-                (Json.Decode.field "data" (Builder.responseDataDecoder request))
+                (Json.Decode.maybe <| Json.Decode.field "errors" Response.errorsDecoder)
+                (Json.Decode.maybe <| Json.Decode.field "data" (Builder.responseDataDecoder request))
     in
-    Http.expectStringResponse toMsg <|
-        \response ->
-            case response of
-                Http.BadUrl_ url ->
-                    Err (HttpError (Http.BadUrl url))
+    case response of
+        Http.BadUrl_ url ->
+            Err (HttpError (Http.BadUrl url))
 
-                Http.Timeout_ ->
-                    Err (HttpError Http.Timeout)
+        Http.Timeout_ ->
+            Err (HttpError Http.Timeout)
 
-                Http.NetworkError_ ->
-                    Err (HttpError Http.NetworkError)
+        Http.NetworkError_ ->
+            Err (HttpError Http.NetworkError)
 
-                Http.BadStatus_ metadata _ ->
-                    Err (HttpError (Http.BadStatus metadata.statusCode))
+        Http.BadStatus_ metadata _ ->
+            Err (HttpError (Http.BadStatus metadata.statusCode))
 
-                Http.GoodStatus_ metadata body ->
-                    case Json.Decode.decodeString decoder body of
-                        Ok ( Just errors, _ ) ->
-                            Err (GraphQLError errors)
+        Http.GoodStatus_ _ body ->
+            case Json.Decode.decodeString decoder body of
+                Ok ( Just errors, _ ) ->
+                    Err (GraphQLError errors)
 
-                        Ok ( Nothing, data ) ->
-                            Ok data
+                Ok ( Nothing, Just data ) ->
+                    Ok data
 
-                        Err err ->
-                            Err (HttpError (Http.BadBody (Json.Decode.errorToString err)))
+                Ok ( Nothing, Nothing ) ->
+                    Err (HttpError (Http.BadBody "`data` and `errors` fields are missing"))
+
+                Err err ->
+                    Err (HttpError (Http.BadBody (Json.Decode.errorToString err)))
 
 
 type alias RequestConfig msg =
@@ -182,19 +268,18 @@ type alias RequestConfig msg =
 
 
 requestConfig :
-    RequestOptions
-    -> String
-    -> Http.Expect a
-    -> Maybe Json.Encode.Value
-    -> RequestConfig a
-requestConfig requestOptions documentString expect variableValues =
+    Maybe BodyPartsConfig
+    -> RequestOptions
+    -> (Result Error result -> msg)
+    -> Builder.Request operationType result
+    -> RequestConfig msg
+requestConfig maybeBodyPartsConfig requestOptions toMsg request =
     let
-        ( url, body ) =
-            if requestOptions.method == "GET" then
-                ( Util.parameterizedUrl requestOptions.url documentString variableValues, Http.emptyBody )
+        expect =
+            expectGraphQL toMsg request
 
-            else
-                ( requestOptions.url, Util.postBody documentString variableValues )
+        { url, body } =
+            httpPayload maybeBodyPartsConfig requestOptions request
     in
     { method = requestOptions.method
     , headers = requestOptions.headers
@@ -204,3 +289,67 @@ requestConfig requestOptions documentString expect variableValues =
     , timeout = requestOptions.timeout
     , tracker = requestOptions.tracker
     }
+
+
+type alias TaskConfig result =
+    { method : String
+    , headers : List Http.Header
+    , url : String
+    , body : Http.Body
+    , resolver : Http.Resolver Error result
+    , timeout : Maybe Float
+    }
+
+
+taskConfig :
+    Maybe BodyPartsConfig
+    -> RequestOptions
+    -> Builder.Request operationType result
+    -> TaskConfig result
+taskConfig maybeBodyPartsConfig requestOptions request =
+    let
+        resolver =
+            resolveGraphQL request
+
+        { url, body } =
+            httpPayload maybeBodyPartsConfig requestOptions request
+    in
+    { method = requestOptions.method
+    , headers = requestOptions.headers
+    , url = url
+    , body = body
+    , resolver = resolver
+    , timeout = requestOptions.timeout
+    }
+
+
+httpPayload :
+    Maybe BodyPartsConfig
+    -> RequestOptions
+    -> Builder.Request operationType result
+    -> { url : String, body : Http.Body }
+httpPayload maybeBodyPartsConfig requestOptions request =
+    let
+        documentString =
+            Builder.requestBody request
+
+        variableValues =
+            Builder.jsonVariableValues request
+    in
+    if requestOptions.method == "GET" then
+        { url = Util.parameterizedUrl requestOptions.url documentString variableValues
+        , body = Http.emptyBody
+        }
+
+    else
+        case maybeBodyPartsConfig of
+            Just filesConfig ->
+                { url = requestOptions.url
+                , body =
+                    Http.multipartBody <|
+                        Http.stringPart filesConfig.jsonPartName (Util.postBodyJson documentString variableValues |> Json.Encode.encode 0)
+                            :: filesConfig.additionalParts
+                }
+
+            Nothing ->
+                { url = requestOptions.url, body = Util.postBody documentString variableValues }
